61c6889688344f1965efea64d0b7bc67522893de
Mridhul <mridhul67@gmail.com>
2024-10-10 23:41:18 +0530

    Write testing code

--- main.mjs
+++ main.mjs
@@ -1,1 +1,12 @@
+import linkedListFactory from "./linked-list-factory.mjs";
 
+const list = new linkedListFactory();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
